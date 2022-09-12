ca10651270c9e9b79abeb608c6fd27b9e39e322f
sumit <sksumitkumardiwaker@gmail.com>
2022-09-12 13:52:27 +0530

    up to razorpay

--- Routers/bookingRouter.js
+++ Routers/bookingRouter.js
@@ -4,8 +4,13 @@ const factory=require("../helper/factory");
 const protectRoute=require("./authRouter");
 const userModel=require("../model/userModel");
 const bookingModel=require("../model/bookingModel");
+const Razorpay=require("razorpay");
+const {KEY_ID,KEY_SECRET}=require("../model/screte");
 // const reviewModel=require("../model/reviewModel");
-
+var razorpay=new Razorpay({
+    key_id:KEY_ID,
+    key_secret:KEY_SECRET,
+});
 
 const initiateBooking=async function(req,res){
 try{
@@ -15,9 +20,25 @@ let userId=req.body.user;
 let user=await userModel.findById(userId);
 user.bookings.push(bookingId);
 await user.save();
+
+const payment_capture=1;
+const amount=500;
+const currency="INR";
+ 
+const options={
+    amount,
+    currency,
+    receipt:`rs_${bookingId}`,
+    payment_capture,
+};
+const response=await razorpay.orders.create(options);
+console.log(response);
 res.status(200).json({
+    id:response.id,
+    currency:response.currency,
+    amount:response.amount,
+    booking:booking,
     message:"booking created",
-    booking:booking
 })
 }catch(err){
 res.status(500).json({
@@ -47,7 +68,24 @@ const deletebooking=async function(req,res){
     }
 const updatebooking =factory.updateElement(bookingModel);
 const getbookingById=factory.getElementById(bookingModel); 
+async function verifyPayment(req,res){
+    const secret=KEY_SECRET;
+    console.log(req.body);
+    const shasum=crypto.createHmac("sha256",secret);
+    shasum.update(JSON.stringify(req.body));
 
+    console.log(digest, req.headers["x-razorpay-signature"]);
+
+    if (digest === req.headers["x-razorpay-signature"]) {
+      console.log("request is legit");
+      res.status(200).json({
+        message: "OK",
+      });
+    } else {
+      res.status(403).json({ message: "Invalid" });
+    }
+};
+bookingRouter.route("/verification").post(verifyPayment);
 bookingRouter.use(protectRoute);
 bookingRouter.route("/") 
 // add protectRoute in that
--- model/screte.js
+++ model/screte.js
@@ -1,5 +1,7 @@
 module.exports={
     DB_Link:"mongodb+srv://admin:teMXgstk8gkT9GWp@cluster0.tt3ppsf.mongodb.net/?retryWrites=true&w=majority",
     JWT_Key:"dakjdkhwjdwhdkedkmdk",
-    APP_PASSWORD: "abc" // put your email generated password
+    APP_PASSWORD: "abc", // put your email generated password
+    KEY_ID:"rzp_test_ijTH40fBEAJ8qJ",
+    KEY_SECRET:"jeUVqUP4n1KTFVsG4B67TwHA",
 }
